a5d61a6e1b1c7ea1c56c526129c3d4d872672aac
Alexander Larsson <alexl@redhat.com>
2023-06-20 10:00:20 +0200

    initial version

---
+++ runkernel-xfstest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" != 1 ]; then
+    echo No kernel image specified
+    exit 1
+fi
+
+KERNEL=$1
+
+qemu-img create -f qcow2 test.qcow2 10G
+qemu-img create -f qcow2 test-scratch.qcow2 10G
+
+mkdir -p shared
+/usr/libexec/virtiofsd --socket-path=.vhostqemu -o source=shared -o cache=always &> /dev/null &
+
+qemu-kvm --nographic  -smp 8 -enable-kvm -m 4G -cpu host -drive file=composefs.qcow2,index=0,media=disk,format=qcow2,if=virtio,snapshot=off -drive file=test.qcow2,index=1,media=disk,format=qcow2,if=virtio,snapshot=off -drive file=test-scratch.qcow2,index=2,media=disk,format=qcow2,if=virtio,snapshot=off  --kernel $KERNEL --append "root=/dev/vda1 console=ttyS0 ro" -chardev socket,id=char0,path=.vhostqemu -device vhost-user-fs-pci,queue-size=1024,chardev=char0,tag=sharedfs -object memory-backend-file,id=mem,size=4G,mem-path=/dev/shm,share=on -numa node,memdev=mem
